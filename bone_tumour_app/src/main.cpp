3852632e0999dc96e62d244c57b039c5db021263
Miguel De Sousa <99416503+Miguel-De-Sousa@users.noreply.github.com>
2026-06-04 16:41:44 +0100

    simple qwidget gui init

--- bone_tumour_app/src/main.cpp
+++ bone_tumour_app/src/main.cpp
@@ -2,6 +2,10 @@
 #include <QWidget>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QStackedWidget>
+#include <QComboBox>
+#include <QObject>
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -17,6 +21,25 @@ int main(int argc, char *argv[]) {
 
     titleLabel->setStyleSheet("font-size: 18px; font-weight: bold; color: white;");
     layout->addWidget(titleLabel);
+
+    QWidget *firstPageWidget = new QWidget;
+    QWidget *secondPageWidget = new QWidget;
+
+    QStackedWidget *stackedWidget = new QStackedWidget;
+    stackedWidget->addWidget(firstPageWidget);
+    stackedWidget->addWidget(secondPageWidget);
+    layout->addWidget(stackedWidget);
+
+    QComboBox *pageComboBox = new QComboBox;
+    pageComboBox->addItem(QObject::tr("Page 1"));
+    pageComboBox->addItem(QObject::tr("Page 2"));
+    pageComboBox->addItem(QObject::tr("Page 3"));
+    QObject::connect(pageComboBox, &QComboBox::activated,
+            stackedWidget, &QStackedWidget::setCurrentIndex);
+    
+    QPushButton *uploadButton = new QPushButton("Upload Image", firstPageWidget);
+    uploadButton->setIcon(QIcon("/Users/miguel/GitHub/bone_tumour_classification/bone_tumour_app/assets/downloads.png"));
+    uploadButton->setIconSize(QSize(16,16));
 
     mainWindow.show();
 
